d85709bae215f6254e9e8e841c72de4629086a7d
nlc <NineLifeCat@yandex.ru>
2020-12-29 00:38:51 +0300

    added fillExpandedSearchTest

--- src/test/java/B2bCenterTests.java
+++ src/test/java/B2bCenterTests.java
@@ -9,7 +9,7 @@ import static io.qameta.allure.Allure.step;
 public class B2bCenterTests extends TestBase{
 
     @Test
-    @DisplayName("поиск компании по слову - качество")
+    @DisplayName("Поиск компании по слову")
     void searchTest(){
         step("Открываем главную страницу b2b-center", () -> {
                 open("https://www.b2b-center.ru");
@@ -24,4 +24,34 @@ public class B2bCenterTests extends TestBase{
             $("[class='table table-hover table-filled search-results']").shouldHave(text("ООО \"ТД ПОЛИМЕТАЛЛ\""));
         });
     }
+
+    @Test
+    @DisplayName("Заолнение критериев расширенного поиска")
+    void fillExpandedSearchTest(){
+        step("открываем главную страницу b2b-center", () -> {
+            open("https://www.b2b-center.ru");
+                });
+        step("Проверям наличие фразы Торги по отрослям", () -> {
+           $("[class='wrapper neutralBg']").shouldHave(text("Торги по отраслям"));
+        });
+        step("Вводим в поисковую строку слово и нажимаем Enter", () -> {
+           $("#f_keyword_above").val("золото").pressEnter();
+        });
+        step("Ищем в результатах поиска компанию", () -> {
+            $("[class='table table-hover table-filled search-results']").shouldHave(text("ОАО \"ЗОЛОТО СЕЛИГДАРА\""));
+            System.out.println("shti");
+        });
+    }
 }
+//2. fillExpandedSearchTest
+//- open main page
+//- assert main page - торги по отрослям
+//- setValue on navigation bar - золото
+//- assert ОАО "ЗОЛОТО СЕЛИГДАРА"
+//- on expanded search menu chose:
+//- example - трубы
+//- add name of region city Moscow
+//- coast lot down - up 100_000  100_000_000
+//- press submit filters
+//- assert ООО "ТД "ТТК"
+
