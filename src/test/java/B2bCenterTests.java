23e292017b0d6a3a2099351015e1f1ff9da785b8
nlc <NineLifeCat@yandex.ru>
2021-01-02 12:38:11 +0300

    refactor code

--- src/test/java/B2bCenterTests.java
+++ src/test/java/B2bCenterTests.java
@@ -14,11 +14,11 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 
-public class B2bCenterTests extends TestBase{
+public class B2bCenterTests extends TestBase {
 
     Faker faker = new Faker(new Locale("ru"));
     FakeValuesService fakeValuesService = new FakeValuesService(
-            new Locale("ru"), new RandomService());
+            new Locale("en-GB"), new RandomService());
     String firstName = faker.name().firstName();
     String lastName = faker.name().lastName();
     String email = fakeValuesService.bothify("???##@gmail.com");
@@ -26,19 +26,23 @@ public class B2bCenterTests extends TestBase{
     String comment = faker.lorem().paragraph();
 
     String mainPageB2bCenter = "https://www.b2b-center.ru";
+    String selfEducationB2bCenterPage = "https://www.b2b-center.ru/training/?t=self_education#self_education";
 
     @Test
     @DisplayName("Поиск компании по слову")
-    void searchTest(){
+    void searchTest() {
         step("Открываем главную страницу b2b-center", () -> {
                 open(mainPageB2bCenter);
         });
+
         step("Проверяем наличие фразы Корпоративные закупки и продажи онлайн", () -> {
                 $("[class='pull-left']").shouldHave(text("Корпоративные закупки и продажи онлайн"));
         });
+
         step("Вводим в поисковую строку слово и нажимаем Enter", () -> {
             $("#f_keyword_above").val("качество").pressEnter();
         });
+
         step("Ищем в результатах поиска компанию", () -> {
             $("[class='table table-hover table-filled search-results']").shouldHave(text("ООО \"ТД ПОЛИМЕТАЛЛ\""));
         });
@@ -46,45 +50,53 @@ public class B2bCenterTests extends TestBase{
 
     @Test
     @DisplayName("Заполнение критериев расширенного поиска")
-    void fillExpandedSearchTest(){
+    void fillExpandedSearchTest() {
         step("открываем главную страницу b2b-center", () -> {
             open(mainPageB2bCenter);
-                });
+        });
+
         step("Проверям наличие фразы Торги по отрослям", () -> {
            $("[class='wrapper neutralBg']").shouldHave(text("Торги по отраслям"));
         });
+
         step("Вводим в поисковую строку слово и нажимаем Enter", () -> {
            $("#f_keyword_above").val("золото").pressEnter();
         });
+
         step("Ищем в результатах поиска компанию", () -> {
             $("[class='table table-hover table-filled search-results']").shouldHave(text("ОАО \"ЗОЛОТО СЕЛИГДАРА\""));
         });
+
         step("Выбираем дефолтное значение в блоке 'Например'", () -> {
             $("[class='apseudo search-example']").shouldHave(text("трубы")).click();
-                });
+        });
+
         step("Вводим название региона ", () -> {
             $("[class='selectize-ico selectize-ico-add']").click();
             $("[class='selectize-option js-active']").shouldHave(text("Россия")).click();
             $("[class='selectize-ico selectize-ico-add']").click();
         });
+
         step("Устанавливаем ценовой диапазон от и до", () -> {
             $(byName("price_start")).val("100000");
             $(byName("price_end")).val("100000000");
         });
+
          step("Нажимаем на кнопку 'Применить фильтры'", () -> {
              $("#submit_filter_button").click();
              $("[class='table table-hover table-filled search-results']").shouldHave(text("ООО \"ТД ПОЛИМЕТАЛЛ\""));
-                });
+         });
     }
 
-    @Disabled
+    @Disabled("Тест игнорируется, что-бы не спамить на проде")
     @Test
     @DisplayName("Заполнение формы 'Задайте вопрос по обучению'")
-    void fillEducationQuestionFormTest(){
+    void fillEducationQuestionFormTest() {
         step("Открываем страницу с формой", () -> {
-            open("https://www.b2b-center.ru/training/?t=self_education#self_education");
+            open(selfEducationB2bCenterPage);
             $("[class='wrapper featured-layout']").shouldHave(text("Обучение закупкам, торгам по 223 ФЗ с нуля. Повышение квалификации в сфере закупок на B2B-Center"));
         });
+
         step("Заолняем форму 'Задайте вопрос по обучению'", () -> {
            $(byName("request_phone")).val(telephoneNumber);
            $(byName("request_name")).val(firstName + " " + lastName);
